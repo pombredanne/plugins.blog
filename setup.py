b0ea9b4fd9df7c51659afcab48a39be04045de92
Schon Peter <slapec@codehouse.hu>
2015-06-22 14:46:06 +0200

    pep8

--- setup.py
+++ setup.py
@@ -12,17 +12,14 @@ standard_exclude = ('*.py', '*.pyc', '*$py.class', '*~', '.*', '*.bak', 'example
 standard_exclude_directories = ('.*', 'CVS', '_darcs', './build',
                                 './dist', 'EGG-INFO', '*.egg-info')
 
+
 # (c) 2005 Ian Bicking and contributors; written for Paste (http://pythonpaste.org)
 # Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
 # Note: you may want to copy this into your setup.py file verbatim, as
 # you can't import this from another package, when you don't know if
 # that package is installed yet.
-def find_package_data(
-    where = '.', package = '',
-    exclude = standard_exclude,
-    exclude_directories = standard_exclude_directories,
-    only_in_packages = False,
-    show_ignored = False):
+def find_package_data(where='.', package='', exclude=standard_exclude, exclude_directories=standard_exclude_directories,
+                      only_in_packages=False, show_ignored=False):
     """
     Return a dictionary suitable for use in ``package_data``
     in a distutils ``setup.py`` file.
@@ -59,18 +56,14 @@ def find_package_data(
             if os.path.isdir(fn):
                 bad_name = False
                 for pattern in exclude_directories:
-                    if (fnmatchcase(name, pattern)
-                        or fn.lower() == pattern.lower()):
+                    if fnmatchcase(name, pattern) or fn.lower() == pattern.lower():
                         bad_name = True
                         if show_ignored:
-                            print >> sys.stderr, (
-                                "Directory %s ignored by pattern %s"
-                                % (fn, pattern))
+                            print("Directory %s ignored by pattern %s" % (fn, pattern), file=sys.stderr)
                         break
                 if bad_name:
                     continue
-                if (os.path.isfile(os.path.join(fn, '__init__.py'))
-                    and not prefix):
+                if os.path.isfile(os.path.join(fn, '__init__.py')) and not prefix:
                     if not package:
                         new_package = name
                     else:
@@ -82,19 +75,15 @@ def find_package_data(
                 # is a file
                 bad_name = False
                 for pattern in exclude:
-                    if (fnmatchcase(name, pattern)
-                        or fn.lower() == pattern.lower()):
+                    if fnmatchcase(name, pattern) or fn.lower() == pattern.lower():
                         bad_name = True
                         if show_ignored:
-                            print >> sys.stderr, (
-                                "File %s ignored by pattern %s"
-                                % (fn, pattern))
+                            print("File %s ignored by pattern %s" % (fn, pattern), sys.stderr)
                         break
                 if bad_name:
                     continue
                 out.setdefault(package, []).append(prefix + name)
     return out
-
 
 
 LONG_DESCRIPTION = """
@@ -105,12 +94,12 @@ Blog plugin for Bangoo CMS system
 """
 
 setup(
-    name = 'bangoo.plugins.blog',
+    name='bangoo.plugins.blog',
     namespace_packages=['bangoo.plugins'],
-    version = version,
-    description = "Blog plugin for Bangoo CMS system.",
-    long_description = LONG_DESCRIPTION,
-    classifiers = [
+    version=version,
+    description="Blog plugin for Bangoo CMS system.",
+    long_description=LONG_DESCRIPTION,
+    classifiers=[
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Framework :: Django",
@@ -118,14 +107,14 @@ setup(
         "Operating System :: OS Independent",
         "Natural Language :: English",
     ],
-    keywords = ['django', 'bangoo', 'blog'],
-    author = 'Lovas Bence',
-    author_email = 'lovas.bence@codehouse.hu',
-    url = 'http://lovasb.com',
-    license = 'proprietary',
-    packages = find_packages(),
-    package_data = find_package_data("bangoo", only_in_packages = False),
-    include_package_data = True,
-    zip_safe = False,
-    install_requires = ['setuptools'], #, 'bangoo'],
+    keywords=['django', 'bangoo', 'blog'],
+    author='Lovas Bence',
+    author_email='lovas.bence@codehouse.hu',
+    url='http://lovasb.com',
+    license='proprietary',
+    packages=find_packages(),
+    package_data=find_package_data("bangoo", only_in_packages = False),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['setuptools'],  # , 'bangoo'],
 )
